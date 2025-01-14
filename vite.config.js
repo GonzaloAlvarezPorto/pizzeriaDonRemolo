4f628a7648b9fc1b4a5a6f334a189f75b45d09f3
GonzaloAlvarezPorto <gonzaalvarezporto@gmail.com>
2025-01-14 09:47:42 -0300

    volvimos al origen

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'public', // Verifica que la carpeta 'front/dist' sea la correcta
-  },
   server: {
     proxy: {
-      '/api': 'http://localhost:8080',
-    },
-  },
+      '/api': 'http://localhost:8080'
+    }
+  }
 });
