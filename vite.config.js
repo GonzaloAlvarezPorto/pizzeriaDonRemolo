f5bdc2eb56244401f1babfadc5570570293cc93c
GonzaloAlvarezPorto <gonzaalvarezporto@gmail.com>
2025-01-14 09:44:54 -0300

    Conchita Barreda

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'front/dist', // Verifica que la carpeta 'front/dist' sea la correcta
+    outDir: 'public', // Verifica que la carpeta 'front/dist' sea la correcta
   },
   server: {
     proxy: {
